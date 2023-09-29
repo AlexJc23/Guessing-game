a9c7f5027b79d53fdf9480eac7e9d4f5cb0ac3bb
Alex Carl <alexjcarl97@gmail.com>
2023-09-28 21:36:12 -0400

    Guessing game

--- guessing-game.js
+++ guessing-game.js
@@ -5,45 +5,49 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+//global varibles
 let secretNumber;
+let numAttempts;
 
-//generates a random number between two numbers
+
 let randomInRange = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     secretNumber = Math.floor(Math.random() * (max - min) + min)
-    // console.log(secretNumber)
     return secretNumber
 }
 
+const askLimit = () => {rl.question("How many attempts do you need? ", attempts => {
+        numAttempts = Number(attempts)
+        askRange()
+})
+}
 
-
-
-const askRange = () => {rl.question("Pick a min number... ", minNumber => {
-
-    rl.question("Pick a max number... ", maxNumber => {
+const askRange = () => {rl.question("Pick a minimum number... ", minNumber => {
+    rl.question("Pick a maximum number... ", maxNumber => {
         console.log(`I'm thinking of a number between ${minNumber} and ${maxNumber}`)
         randomInRange(Number(minNumber), Number(maxNumber))
         askGuess()
-
     })
 })
 }
 
-
-
-
 const askGuess = () => {rl.question("Guess a number... ", answer => {
-    console.log(`Your guess was ${answer}`, );
-    if(!checkGuess(Number(answer))) {
-        return askGuess()
-    }
-    console.log(`You Won!
-    Your prize is nothing!`)
-    rl.close();
-});
+    console.log("Attempts left ", numAttempts)
+        console.log(`Your guess was ${answer}`, );
+        if(numAttempts === 1) {
+            console.log(`You Lost bro, the number was ${secretNumber}`)
+            return rl.close();
+        }
+        if(!checkGuess(Number(answer))) {
+            return askGuess(numAttempts -= 1)
+        } else {
+        console.log(`You Won!
+        Your prize is nothing!`)
+        return rl.close();
+        }
+    });
 }
-
 
 const checkGuess = (num) => {
     if(secretNumber === num) {
@@ -59,4 +63,4 @@ const checkGuess = (num) => {
     }
 }
 
-askRange()
+askLimit()
